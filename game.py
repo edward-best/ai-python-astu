8b4f0a5fdc22c221653572a7d147bd387ab26849
edward-best <eduardbek88@gmail.com>
2024-10-04 14:33:10 +0400

    Добавил внутриигровой цикл

--- game.py
+++ game.py
@@ -1,4 +1,28 @@
 from color import Color
+import rules
 
 def session(board: list[list[Color]], first_bot, second_bot) -> dict:
+    turn_deque = [first_bot, second_bot]
+    color_deque = [Color.BLACK, Color.WHITE]
+    turn_index = 0
+    first_stopped = False
+    while True:
+        current_bot = turn_deque[turn_index]
+        current_color = color_deque[turn_index]
+        fields = rules.available_fields()
+        if len(fields) == 0:
+            if not first_stopped:
+                first_stopped = True
+                turn_index = (turn_index + 1) % 2
+                continue
+            else:
+                break
+        old_board = board
+        chosen_field = current_bot(board, current_color)
+        if rules.board_diff(old_board, board) or not rules.check_field_validness(chosen_field) \
+            or chosen_field is None and len(fields) != 0 or not rules.turn_validness(chosen_field, fields):
+            break
+        board[chosen_field[0], chosen_field[1]] = current_color
+        turn_index = (turn_index + 1) % 2
+    color_counts = rules.count_fields(board)
     return {"winner" : "not you"}
--- rules.py
+++ rules.py
@@ -1,19 +1,24 @@
 from color import Color
 
-def board_diff(old: list[list[Color]], current: list[list[Color]]) -> int:
-    total = 0
+"""
+Определяет наличие изменений, сделанных на доске ботом
+"""
+def board_diff(old: list[list[Color]], current: list[list[Color]]) -> bool:
     for i in range(8):
         for j in range(8): 
             if not old[i][j] is current[i][j]:
-                total += 1
-    return total
-
-def check_turn_validness(turn_made: tuple[int, int], allowed: list[tuple[int, int]]) -> bool:
-    for expected_turn in allowed:
-        if turn_made == expected_turn:
-            return True
+                return True
     return False
 
+"""
+Определяет допустимость хода при известном наборе
+"""
+def turn_validness(turn_made: tuple[int, int], allowed: list[tuple[int, int]]) -> bool:
+    return turn_made in allowed
+
+"""
+Возвращает координаты полей, имеющих выбранный цвет
+"""
 def color_fields(board: list[list[Color]], current_color) -> list[tuple[int, int]]:
     result = []
     for i in range(8):
@@ -22,9 +27,15 @@ def color_fields(board: list[list[Color]], current_color) -> list[tuple[int, int
                 result.append((i, j))
     return result
 
+"""
+Проверяет выход координаты за пределы доски
+"""
 def check_field_validness(field: tuple[int, int]) -> bool:
     return (field[0] >= 0 and field[0] < 8) and (field[1] >= 0 and field[1] < 8)
 
+"""
+Возвращает список координат, куда можно поставить фишку выбранного цвета
+"""
 def available_fields(board: list[list[Color]], current_color) -> list[tuple[int, int]]:
     result = []
     player_fields = color_fields(board, current_color)
@@ -36,4 +47,14 @@ def available_fields(board: list[list[Color]], current_color) -> list[tuple[int,
                 current_field = (current_field[0] + direction[0], current_field[1] + direction[1])
                 if check_field_validness(current_field) and board[current_field[0], current_field[1]] is Color.EMPTY:
                     result.append(current_field)
+    return result
+
+"""
+Выполняет подсчет полей по цветам
+"""
+def count_fields(board: list[list[Color]]) -> dict:
+    result = {}
+    for i in range(8):
+        for j in range(8):
+            result[board[i][j]] += 1
     return result
